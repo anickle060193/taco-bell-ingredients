9b925bac833736b032d153a13c069317d62b9f99
Adam Nickle <anickle060193@gmail.com>
2019-09-05 20:53:11 -0500

    Second initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createMuiTheme, MuiThemeProvider, CssBaseline } from '@material-ui/core';
+
 import './App.css';
 
-const App: React.FC = () => {
+const theme = createMuiTheme( {
+  palette: {
+    type: 'dark',
+  },
+} );
+
+const App: React.FC = () =>
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        {JSON.stringify( process.env.DISPLAY_NAME, null, 2 )}
+      </div>
+    </MuiThemeProvider>
   );
-}
+};
 
 export default App;
